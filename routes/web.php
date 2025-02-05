edb2259efa2b8db49699dc965d14b1a56324ba8a
NicoGrosso12 <nicolas.grosso@lightit.io>
2025-02-05 10:18:14 -0300

    feat: models and migration
    feat: models and migrations
    
    feat: models and migrations
    
    feat: models and migrations
    
    feat: models and migrations
    
    feat: models and migrations
    
    feat: models and migrations
    
    feat: models and migrations
    
    feat: models and migrations
    
    feat: models and migrations
    
    feat: models and migrations
    
    feat: models and migrations
    
    feat: models and migrations
    
    feat: models and migrations
    
    feat: models and migrations
    
    feat: models and migrations
    
    feat: models and migrations

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,10 @@
 use Illuminate\Support\Facades\Route;
 use Lightit\Shared\App\Exceptions\InvalidActionException;
 
+Route::get('/', function(){
+    return view('welcome');
+});
+
 Route::get('invalid', static fn() => throw new InvalidActionException("Is not valid"));
 
 Route::get('{unknown}', static fn () => view('app  '))->where('unknown', '^(?!api).*$');
