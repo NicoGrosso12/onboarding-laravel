d3c99f86313be1e2b41bba4b3cd1ff499ec6fbc3
NicoGrosso12 <nicolas.grosso@lightit.io>
2025-02-06 12:40:50 -0300

    feat: models and migrations working well, started frontend configuration

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Route;
 use Lightit\Shared\App\Exceptions\InvalidActionException;
 
 Route::get('/', function(){
-    return view('layouts.app');
+    return view('welcome');
 });
 
 Route::get('invalid', static fn() => throw new InvalidActionException("Is not valid"));
